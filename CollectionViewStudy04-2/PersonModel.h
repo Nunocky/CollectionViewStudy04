0196785ce6e17009994957375a6d79bd964d8369
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2014-05-01 09:59:08 +0900

    04-02

---
+++ CollectionViewStudy04-2/PersonModel.h
@@ -0,0 +1,16 @@
+//
+//  PersonModel.h
+//  CollectionViewStudy04
+//
+//  Created by 布川祐人 on 2014/05/01.
+//  Copyright (c) 2014年 NUNOKAWA Masato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonModel : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *title;
+
+@end
---
+++ CollectionViewStudy04-2Tests/CollectionViewStudy04_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  CollectionViewStudy04_2Tests.m
+//  CollectionViewStudy04-2Tests
+//
+//  Created by 布川祐人 on 2014/05/01.
+//  Copyright (c) 2014年 NUNOKAWA Masato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewStudy04_2Tests : XCTestCase
+
+@end
+
+@implementation CollectionViewStudy04_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
