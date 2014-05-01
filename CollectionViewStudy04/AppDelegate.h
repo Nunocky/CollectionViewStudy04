2f330e930adfb7acf8c17a6eb3be119e950ded7e
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2014-05-01 09:36:05 +0900

    Initial Commit

---
+++ CollectionViewStudy04/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewStudy04
+//
+//  Created by 布川祐人 on 2014/05/01.
+//  Copyright (c) 2014年 NUNOKAWA Masato. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
